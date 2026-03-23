480c019d461fb8a8275357a61bff12605a32fd20
Amisha Sadhukhan <sadhukhanamisha@gmail.com>
2026-03-23 23:32:59 +0530

    Add endpoint to fetch all products

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.dependencies import get_db
+from app.models import Product
 
 app = FastAPI()
 
@@ -6,3 +9,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Price Monitoring API is running"}
+
+
+@app.get("/products")
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(Product).all()
+
+    return [
+        {
+            "id": p.id,
+            "name": p.name,
+            "brand": p.brand,
+            "price": float(p.current_price),
+            "source": p.source
+        }
+        for p in products
+    ]
