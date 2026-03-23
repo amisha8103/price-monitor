671735f54b595d75a74b1b92c3db501d98705ada
Amisha Sadhukhan <sadhukhanamisha@gmail.com>
2026-03-23 23:38:00 +0530

    Add filtering to products endpoint by price and source

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi import Query
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.models import Product
@@ -11,9 +12,26 @@ def root():
     return {"message": "Price Monitoring API is running"}
 
 
+
 @app.get("/products")
-def get_products(db: Session = Depends(get_db)):
-    products = db.query(Product).all()
+def get_products(
+    db: Session = Depends(get_db),
+    source: str = None,
+    min_price: float = None,
+    max_price: float = None
+):
+    query = db.query(Product)
+
+    if source:
+        query = query.filter(Product.source == source)
+
+    if min_price:
+        query = query.filter(Product.current_price >= min_price)
+
+    if max_price:
+        query = query.filter(Product.current_price <= max_price)
+
+    products = query.all()
 
     return [
         {
